4de95df1a25968bb9ab0e96c0bab8ce8a2cdf6ed
Wade Barnes <wade.barnes@shaw.ca>
2021-02-13 06:01:55 -0800

    Update Git refs
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,6 +1,6 @@
 export PROJECT_NAMESPACE="583dbf"
 export GIT_URI="https://github.com/bcgov/representation-grant-app.git"
-export GIT_REF="master"
+export GIT_REF="main"
 
 # The templates that should not have their GIT referances(uri and ref) over-ridden
 # Templates NOT in this list will have they GIT referances over-ridden
