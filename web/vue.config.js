e0689b976eaf5573061c151a2c40b57b86f4b4d1
Travis Semple <travis8814@gmail.com>
2021-08-04 17:40:14 -0700

    Add in fork-ts-checker, disable ts-loader from typechecking.

--- web/vue.config.js
+++ web/vue.config.js
@@ -18,13 +18,15 @@ module.exports = {
   // crossorigin: "anonymous",
   chainWebpack: config => {
     config.module.rules.delete("eslint");
+    config.plugin('fork-ts-checker')
     config.module
       .rule("ts")
       .test(/\.ts$/)
       .use("ts-loader")
       .loader("ts-loader")
       .options({
-        appendTsSuffixTo: [/\.vue$/]
+        appendTsSuffixTo: [/\.vue$/],
+        transpileOnly: true
       });
   },
   parallel: false // https://stackoverflow.com/questions/59951379/vue-cli-upgrade-from-v3-to-v4-breaks-build-process-with-thread-loader-error-can
