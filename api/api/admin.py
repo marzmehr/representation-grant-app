b554b5f334eb27cad126055746878ebd39d7d90b
Travis Semple <travis8814@gmail.com>
2021-09-03 13:23:02 -0700

    Add admin back into REPGRANT.

--- api/api/admin.py
+++ api/api/admin.py
@@ -1,24 +1,48 @@
-"""
-    REST API Documentation for Family Law Act
-
-    OpenAPI spec version: v1
-
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-"""
-
 from django.contrib import admin
-
 from .models.user import User
+from django.apps import apps
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    exclude = [
+        "authorization_guid",
+        "authorization_directory",
+        "authorization_email",
+        "groups",
+        "user_permissions",
+        "password",
+        "display_name",
+        "accepted_terms",
+    ]
+    list_display = [
+        "first_name",
+        "last_name",
+        "email",
+        "is_superuser",
+        "is_staff",
+        "is_active",
+        "date_joined",
+        "universal_id",
+        "idir_userid"
+    ]
+
+    # Disable adding, because logging in should seed the user.
+    def has_add_permission(self, request):
+        return False
+
+    """ Disable deleting, setting to inactive is better
+    otherwise next time the user uses SSO,
+    they will repopulate. """
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+
+# De-register all models for all apps.
+for app_config in apps.get_app_configs():
+    for model in app_config.get_models():
+        if admin.site.is_registered(model):
+            admin.site.unregister(model)
+
+admin.site.register(User, UserAdmin)
+admin.site.site_header = "REPGRANT Administration"
