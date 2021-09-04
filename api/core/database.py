ed88bec3b8fdfc60f71d5da39fd0704510ec1f11
Travis Semple <travis8814@gmail.com>
2021-09-03 17:20:18 -0700

    Remove staticfiles/Text. Figure out workaround for needing SQLLite to generate static files.

--- api/core/database.py
+++ api/core/database.py
@@ -23,7 +23,7 @@ def config():
     service_name = os.getenv("DATABASE_SERVICE_NAME", "").upper().replace("-", "_")
 
     if service_name:
-        engine = engines.get(os.getenv("DATABASE_ENGINE"), engines["sqlite"])
+        engine = engines.get(os.getenv("DATABASE_ENGINE"), engines["postgresql"])
     else:
         engine = engines["sqlite"]
 
