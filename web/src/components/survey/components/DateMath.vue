14e03f534e80aafbd7ec7e98bf3b22a260a1dd99
nikola-rados <nrados@uvic.ca>
2021-10-29 14:02:43 -0700

    Add easter related holiday handler

--- web/src/components/survey/components/DateMath.vue
+++ web/src/components/survey/components/DateMath.vue
@@ -76,14 +76,46 @@ export default defineComponent({
           return holidates;
         };
 
+        const getEaster = year => {
+          // taken from https://gist.github.com/johndyer/0dffbdd98c2046f41180c051f378f343
+          const f = Math.floor,
+            G = year % 19,
+            C = f(year / 100),
+            H = (C - f(C / 4) - f((8 * C + 13) / 25) + 19 * G + 15) % 30,
+            I = H - f(H / 28) * (1 - f(29 / (H + 1)) * f((21 - G) / 11)),
+            J = (year + f(year / 4) + I + 2 - C + f(C / 4)) % 7,
+            L = I - J,
+            month = 3 + f((L + 40) / 44),
+            day = L + 28 - 31 * f(month / 4);
+
+          return new Date(year, month - 1, day);
+        };
+
+        const easterRelatedHoliday = (year, dayOfWeek, range) => {
+          // we know easter will always be on a sunday, so we can do simple math when we get the date.
+          const holidates = [];
+          for (let i = -range; i <= range; i++) {
+            const temp = getEaster(year + i);
+
+            if (dayOfWeek === 1) {
+              temp.setDate(temp.getDate() + 1);
+              holidates.push(temp);
+            } else if (dayOfWeek === 5) {
+              temp.setDate(temp.getDate() - 2);
+              holidates.push(temp);
+            }
+          }
+          return holidates;
+        };
+
         // for now lets just have a steady default range
         const range = 2;
 
         const holidates = {
           new_year: dateFactory(year, 0, 1, range),
           family_day: dateByRule(year, { month: 1, week: 2, dayOfWeek: 1 }, range),
-          good_friday: [],
-          easter_monday: [],
+          good_friday: easterRelatedHoliday(year, 5, range),
+          easter_monday: easterRelatedHoliday(year, 1, range),
           victoria_day: [],
           canada_day: dateFactory(year, 6, 1, range),
           bc_day: dateByRule(year, { month: 7, week: 0, dayOfWeek: 1 }, range),
