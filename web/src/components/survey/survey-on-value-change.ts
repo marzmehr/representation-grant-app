84c2fcf0ab39e1ebc34692fd15bb75d92dc0c373
Travis Semple <travis8814@gmail.com>
2021-11-16 12:59:44 -0800

    REPGRANT-153 - Rewire up 153, to use panel instead of table.

--- web/src/components/survey/survey-on-value-change.ts
+++ web/src/components/survey/survey-on-value-change.ts
@@ -1,5 +1,8 @@
 //Needs to be function, otherwise this context wont work.
+import { addDays, format, getDay, parseISO } from "date-fns";
 import { ItemValue } from "survey-vue";
+import { DayOfWeek } from "../utils/holiday";
+import { holidaysByDate } from "./survey-init";
 import { getPotentialApplicants, setPotentialApplicants } from "./survey-state";
 
 //Helper function, that either grabs value from the event, or from the survey via getQuestionByName.
@@ -99,8 +102,69 @@ const combineRecipients = (sender, options) => {
   );
 };
 
+const determineEarliestSubmissionDate = (sender, options) => {
+  const questionNamesToWatch = ["p1DeliveryInfoPanel"];
+  if (!questionNamesToWatch.includes(options.name)) return;
+  const p1DeliveryInfoPanelValue = options.value || [];
+  const calculatedDates = [];
+  p1DeliveryInfoPanelValue.forEach(value => {
+    const method = value?.p1DeliveryMethod;
+    let dateServed = parseISO(value?.p1DeliveryDate);
+    const timeOfDay = value?.p1DeliveryTime;
+    if (!method || isNaN(dateServed.getTime())) return;
+    let extraNoticeDays = 21;
+    switch (method) {
+      case "inperson":
+        break;
+      case "electronic":
+      case "mail":
+        /* For physical mail, it is + at least 7 days. 
+           If that 7th day from served date is a Saturday/Sunday/BC Holiday, 
+           needs to be calculating for the next BC business day, 
+           then +21 days on top of that for the countdown. */
+        /* 
+          For electronic/fax service:
+          Saturday/Sunday/BC holiday, date served = next BC business day, then add 21 days on top of that
+          Business day before 4pm, date served is the date served: +0 days, then +21 days on top of that for the countdown.
+          Business day after 4pm, date served = next BC business day, then add 21 days on top of that
+        */
+        let offsetDays = method == "mail" ? 7 : 0;
+        const bcStats = holidaysByDate;
+        let resolvedDate = false;
+        while (!resolvedDate) {
+          let destinationDate = addDays(dateServed, offsetDays);
+          const destinationDayOfWeek = getDay(destinationDate) as DayOfWeek;
+          if (destinationDayOfWeek == DayOfWeek.Saturday) {
+            offsetDays += 2;
+            continue;
+          } else if (destinationDayOfWeek == DayOfWeek.Sunday) {
+            offsetDays += 1;
+            continue;
+          } else if (bcStats.hasOwnProperty(format(destinationDate, "yyyy-MM-dd"))) {
+            offsetDays += 1;
+            continue;
+          } else if (method == "electronic" && timeOfDay == "after4pm" && offsetDays == 0) {
+            offsetDays += 1;
+            continue;
+          }
+          resolvedDate = true;
+          extraNoticeDays += offsetDays;
+        }
+        break;
+    }
+    dateServed = addDays(dateServed, extraNoticeDays);
+    calculatedDates.push(dateServed);
+  });
+  if (calculatedDates.length == 0) return false;
+  const earliestSubmissionDate = new Date(Math.max.apply(null, calculatedDates));
+  const earliestSubmissionDateQuestion = sender.getQuestionByName("p1earliestSubmissionDate");
+  earliestSubmissionDateQuestion.title = earliestSubmissionDateQuestion.title.replace("[insert date here]", format(earliestSubmissionDate, "yyyy-MM-dd"));
+  earliestSubmissionDateQuestion.visible = true;
+};
+
 export function onValueChanged(sender, options) {
   populateApplicantInfoPanelAndP1DeliveryInfoPanel(sender, options);
   combinePotentialApplicants(sender, options);
   combineRecipients(sender, options);
+  determineEarliestSubmissionDate(sender, options);
 }
