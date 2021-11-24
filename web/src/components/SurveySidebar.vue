8d0d9800d32bfcd43bbc0485bb7432ceb3d71496
Travis Semple <travis8814@gmail.com>
2021-11-24 10:30:27 -0800

    More minor tweaks.

--- web/src/components/SurveySidebar.vue
+++ web/src/components/SurveySidebar.vue
@@ -102,7 +102,6 @@ export default class SandboxSidebar extends Vue {
   padding: 0;
   top: 0;
   width: $sidebar-width-md;
-  height: 100%;
 }
 @include media-breakpoint-up(lg) {
   .sidebar-container {
