c02a22fc150d3cc60ccbc9cc84a9a3cd220428dd
Travis Semple <travis8814@gmail.com>
2021-12-20 18:42:48 -0800

    Minor tweak.

--- web/src/components/SurveySidebar.vue
+++ web/src/components/SurveySidebar.vue
@@ -102,8 +102,7 @@ export default defineComponent({
     });
 
     const formattedLastSavedDate = computed(() => {
-      if (!!getLastSaved.value) return format(getLastSaved.value, "MMMM d, yyyy - HH:mm");
-      return "";
+      return (!!getLastSaved.value) ? format(getLastSaved.value, "MMMM d, yyyy - HH:mm") : "";
     });
 
     return {
