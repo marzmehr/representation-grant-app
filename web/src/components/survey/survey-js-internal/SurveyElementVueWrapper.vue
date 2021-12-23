985eb314b52df22e0b490d24a5bb18ec6e51bad2
Travis Semple <travis8814@gmail.com>
2021-12-23 12:09:38 -0800

    REPGRANT-180 - Questions to Progressively Appear

---
+++ web/src/components/survey/survey-js-internal/SurveyElementVueWrapper.vue
@@ -0,0 +1,25 @@
+<!-- Extended this, so we can wrap survey-element-vue with animations. --> 
+<template>
+  <transition name="slide-fade" appear mode="out-in">
+    <survey-element-vue :css="css" :survey="survey" :element="element"/>
+  </transition>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+import { SurveyModel, SurveyElement, SurveyElementVue } from "survey-vue";
+
+@Component({
+  components: {
+    SurveyElementVue
+  }
+})
+export class SurveyElementVueWrapper extends Vue {
+  @Prop() css: any;
+  @Prop() survey: SurveyModel;
+  @Prop() element: SurveyElement;
+}
+Vue.component("survey-element", SurveyElementVueWrapper);
+export default SurveyElementVueWrapper;
+</script>
