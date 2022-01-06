7f8560de8088d1b1cb0801279fa02acbc9e5eb84
nikola-rados <nrados@uvic.ca>
2022-01-06 14:59:27 -0800

    Remove arrows from number input

--- web/src/components/survey/AddressInfo.vue
+++ web/src/components/survey/AddressInfo.vue
@@ -323,4 +323,15 @@ export default {
     border: 0;
     box-shadow: none;
 }
+
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+/* Firefox */
+input[type=number] {
+  -moz-appearance: textfield;
+}
 </style>
