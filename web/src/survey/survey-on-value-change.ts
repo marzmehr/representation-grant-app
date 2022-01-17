ce030132b2458c3f82ef0edebbaeaa390440fc94
nikola-rados <nrados@uvic.ca>
2022-01-17 15:19:37 -0800

    Rework question scrolling

--- web/src/survey/survey-on-value-change.ts
+++ web/src/survey/survey-on-value-change.ts
@@ -1,7 +1,7 @@
 //Needs to be function, otherwise this context wont work.
 import { notifyP1DeliveryInfoPanel, SurveyQuestionNames } from "@/types/survey-primary";
 import { addDays, format, getDay, parseISO } from "date-fns";
-import { ItemValue, QuestionSignaturePadModel } from "survey-vue";
+import { ItemValue } from "survey-vue";
 import {
   getPotentialApplicants,
   setApplicants,
@@ -10,8 +10,6 @@ import {
   setPrevAddresses,
   setRecipients
 } from "@/state/survey-state";
-import { parseWithOptions } from "date-fns/fp";
-import differenceInQuartersWithOptions from "date-fns/esm/fp/differenceInQuartersWithOptions/index.js";
 
 //Helper function, that either grabs value from the event, or from the survey via getQuestionByName.
 const getValueFromOptionsOrGetQuestion = (sender, options, questionName: string, getText?: boolean) => {
@@ -252,27 +250,23 @@ export const collectPrevAddresses = (sender) => {
   setPrevAddresses(uniqueAddressQuestions);
 };
 
-export const setFocus = (sender, options) => {
-  console.log("We are doing focus-related things");
-  const jumpOffQuestions = [
-    'radiogroup',
-    'dropdown',
-    'checkbox',
-    'yesno'
-  ];
+export const toNextQuestion = (sender, options) => {
   const questionsOnPage = sender.activePage.questions;
   const questionNames = questionsOnPage.map(q => q.name);
-  // const allQuestions = sender.();
   const targetName = options.name;
-  const questionType = options.question.getType();
-  // console.log("all questions");
-  // console.log(allQuestions[7].focus());
-
   const idx = questionNames.indexOf(targetName);
-  if (questionNames.includes(targetName) && questionNames.length - 1 > idx) {
-    console.log("this question is legit");
-    questionsOnPage[idx + 1].focus()
-    console.log(questionsOnPage[idx + 1]);
+  const currQuestion = questionsOnPage[idx];
+  
+  if (currQuestion.getType() !== "address") {
+    for (let i = idx + 1; i < questionNames.length; i++) {
+      if ((questionsOnPage[i].getType() === "infotext" && questionsOnPage[i].isRequired) || (questionsOnPage[i].getType !== "infotext" && questionsOnPage[i].getType() !== "helptext")) {
+        let nextQuestion = document.getElementsByName(questionNames[i]);
+        nextQuestion.forEach(element => {
+          if (element.className === "sv_qstn") element.scrollIntoView({behavior: "smooth", block: "center"});
+        });
+        break;
+      }
+    }
   }
 };
 
@@ -283,5 +277,5 @@ export function onValueChanged(sender, options) {
   determineEarliestSubmissionDate(sender, options);
   setLastUpdated(new Date());
   collectPrevAddresses(sender);
-  setFocus(sender, options);
+  toNextQuestion(sender, options);
 }
