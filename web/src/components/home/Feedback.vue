ba818955b98251ed93c8cf97b6a1b11965a320c5
nikola-rados <nrados@uvic.ca>
2022-01-20 14:02:32 -0800

    Another test

--- web/src/components/home/Feedback.vue
+++ web/src/components/home/Feedback.vue
@@ -91,13 +91,11 @@
 
 <script lang="ts">
 import { defineComponent, ref } from "@vue/composition-api";
-import Tooltip from "@/components/survey/Tooltip.vue";
-import { VueRecaptcha } from 'vue-recaptcha';
+import VueRecaptcha from 'vue-recaptcha';
 import { SurveyDataService } from "@/services/survey-data-service";
 import vueRecaptchaSiteKey from "../../../app.config";
 export default defineComponent({
   components: {
-    Tooltip,
     VueRecaptcha
   },
   name: "feedback",
