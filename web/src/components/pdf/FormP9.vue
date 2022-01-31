b8cc287e02620989fcab199a96a3c5d0dd9f3676
nikola-rados <nrados@uvic.ca>
2022-01-31 14:22:53 -0800

    Initial fixes to styling

--- web/src/components/pdf/FormP9.vue
+++ web/src/components/pdf/FormP9.vue
@@ -1,5 +1,5 @@
 <template>
-  <div key="getLastUpdated">
+  <div key="getLastUpdated" style="font-size:11pt;">
     <div>
       <b-button style="transform:translate(500px,0px)" variant="success" @click="onPrint()">
         Print
@@ -13,201 +13,120 @@
         ref="root"
         style="border:1px solid; border-radius:5px;padding:3rem 4rem 2rem 4rem;"
       >
-        <div style="text-align:center;margin:1rem 0 0 0rem;font-weight: 300;font-size:20px;">
-          FORM P9 (RULE 25-3 (2))
+        <div style="text-align:center;margin:1rem 0 0 0rem;f">
+          <b>FORM P9</b> (RULE 25-3 (2))
         </div>
 
         <div class="mt-3 m-0 p-0 row">
-          <div style="margin-left: 450px; text-align: right; font-size:10px !important">
+          <div style="margin-left: 396px; text-align: right;!important">
             <div>
+              This is the <b>1st</b> affidavit of <b>{{ applicant.fullName }}</b> in this case
+            </div>
+            <div class="mt-2">
+              <underline-form textwidth="5rem" beforetext="and was made on" text="" />
+            </div>
+            <div class="mt-2">
+              <b>{{ applicant.courthouse }}</b>
               <underline-form
-                style="position: static !important"
-                textwidth="8rem"
-                beforetext="This is the"
-                text="1st"
+                textwidth="5rem"
+                beforetext="Registry No."
+                text=""
               />
-              <div style="display:inline-block; margin:0 0 0 0.5rem; padding:0;">
-                affidavit
-              </div>
-            </div>
-            <div class="mt-2">
-              <underline-form textwidth="14rem" beforetext="of" :text="applicant.fullName" />
-              <br/>
-              <div style="display:inline-block; margin:0 0 0 0rem; padding:0;">
-                in this case and was made on
-              </div>
-            </div>
-            <div class="mt-2">
-              <underline-form textwidth="14rem" beforetext="" text="" />
-            </div>
-            <div class="mt-2">
-              <underline-form
-                textwidth="11rem"
-                beforetext=""
-                :text="applicant.courthouse"
-              />
-              <div style="display:inline-block; margin:0 0 0 0.5rem; padding:0;">
-                Registry
-              </div>
-            </div>
-            <div class="mt-2">
-              <underline-form textwidth="12rem" beforetext="No." text="" />
             </div>
           </div>
         </div>
 
-        <div style="text-align:center;margin:1rem 0 1rem 0rem;font-weight: 600;font-size:20px;">
-          <i>IN THE SUPREME COURT OF BRITISH COLUMBIA</i>
+        <div style="text-align:center;margin:1rem 0 1rem 0rem;">
+          In the Matter of the Estate of <b>{{ deceased }}</b>, deceased
         </div>
-        <div style="text-align:center">
-          <div style="display:inline-block;">
-            <i>In the Matter of the Estate of</i>
+        <div style="text-align:center;margin:1rem 0 1rem 0rem;">
+          <b>AFFIDAVIT OF DELIVERY</b>
+        </div>
+
+        <div>
+          I, <b>{{ applicant.fullName }}</b>, of <b>{{ applicant.address }}</b>, 
+          <b>{{ applicant.occupation }}</b>, SWEAR (OR AFFIRM) THAT:
+        </div>
+        <br/>
+        <div style="display:inline;">
+          1 Attached to this affidavit and marked as Exhibit A is a copy of a notice of proposed
+          application in Form P1 (the "notice").
+        </div>
+
+        <div
+          v-if="
+            mailRecipients(applicant).length > 0 ||
+              electronicRecipients(applicant).length > 0 ||
+              inPersonRecipients(applicant).length > 0
+          "
+          class="mt-4 text-justify"
+        >
+          <div>
+            2 I delivered a copy of the notice to the following persons as follows:
           </div>
-          <underline-form textwidth="17rem" beforetext="" :text="deceased" />
-          <div style="display:inline-block; text-indent: 5px;">
-            <i>, deceased</i>
+
+          <div v-if="mailRecipients(applicant).length > 0">
+            <div style="text-indent: 50px;">
+              by mailing it/them to the following persons by ordinary mail:
+            </div>
+            <template v-for="(recipient, i) of mailRecipients(applicant)">
+              <div :key="i" style="display:inline-block; margin:0.5rem 0;text-indent: 50px;">
+                <u><b>{{ recipient.recipientName }}</b> on <b>{{ recipient.p1DeliveryDate }}</b></u>
+              </div>
+            </template>
           </div>
-        </div>
-        <div style="text-align:center;margin:1rem 0 1rem 0rem;font-weight: 600;font-size:18px;">
-          AFFIDAVIT OF DELIVERY
-        </div>
 
-        <div style="display:inline-block; text-indent: 5px;">I</div>
-        <div style="display:inline-block; margin:0.5rem 0;">
-          <div style="display:inline-block; width:0.5rem;"></div>
-          <underline-form textwidth="12rem" beforetext="" :text="applicant.fullName" />
-          <underline-form textwidth="15rem" beforetext=", of" :text="applicant.address" />
-          <underline-form textwidth="7rem" beforetext=", " :text="applicant.occupation" />
-          <underline-form textwidth="0rem" beforetext=", " text="" />
-        </div>
-        <div style="margin:0.25rem 0 1rem 0rem;font-weight: 300;font-size:18px;">
-          SWEAR (OR AFFIRM) THAT:
-        </div>
-
-        <ol style="margin:0rem 0 0 0rem;">
-          <li class="mt-4 text-justify">
-            <div style="display:inline;">
-              Attached to this affidavit and marked as Exhibit A is a copy of a notice of proposed
-              application in Form P1 (the "notice").
+          <div v-if="inPersonRecipients(applicant).length > 0">
+            <div style="text-indent: 50px;">
+              by handing it/them to and leaving it/them with the following persons:
             </div>
-          </li>
-
-          <li
-            v-if="
-              mailRecipients(applicant).length > 0 ||
-                electronicRecipients(applicant).length > 0 ||
-                inPersonRecipients(applicant).length > 0
-            "
-            class="mt-4 text-justify "
-          >
-            <div>
-              I delivered a copy of the notice to the following persons as follows:
-            </div>
-
-            <div v-if="mailRecipients(applicant).length > 0">
-              <div style="text-indent: 20px;">
-                by mailing it/them to the following persons by ordinary mail:
+            <template v-for="(recipient, i) of inPersonRecipients(applicant)">
+              <div :key="i" style="display:inline-block; margin:0.5rem 0;text-indent: 50px;">
+                <u><b>{{ recipient.recipientName }}</b> on <b>{{ recipient.p1DeliveryDate }}</b></u>
               </div>
-              <template v-for="(recipient, i) of mailRecipients(applicant)">
-                <div :key="i" style="display:inline-block; margin:0.5rem 0;">
-                  <underline-form
-                    :key="recipient.recipientName"
-                    style="margin:0.5rem 0 ;display:inline-block; text-indent: 20px;"
-                    textwidth="20rem"
-                    beforetext=""
-                    hint=""
-                    :text="recipient.recipientName"
-                  />
-                  <underline-form
-                    :key="recipient.p1DeliveryDate"
-                    style="margin:0.5rem 0 ;display:inline-block;"
-                    textwidth="15rem"
-                    beforetext="on"
-                    :text="recipient.p1DeliveryDate"
-                  />
-                </div>
-              </template>
+            </template>
+          </div>
+
+          <div v-if="electronicRecipients(applicant).length > 0">
+            <div style="text-indent: 50px;">
+              by sending it/them to the following persons by e-mail, fax or other electronic means 
+              to that person:
             </div>
-
-            <div v-if="inPersonRecipients(applicant).length > 0">
-              <div style="text-indent: 20px;">
-                by handing it/them to and leaving it/them with the following persons:
+            <template v-for="(recipient, i) of electronicRecipients(applicant)">
+              <div :key="i" style="display:inline-block; margin:0.5rem 0;text-indent: 50px;">
+                <u><b>{{ recipient.recipientName }}</b> on <b>{{ recipient.p1DeliveryDate }}</b></u>
               </div>
-              <template v-for="(recipient, i) of inPersonRecipients(applicant)">
-                <div :key="i" style="display:inline-block; margin:0.5rem 0;">
-                  <underline-form
-                    :key="recipient.recipientName"
-                    style="margin:0.5rem 0 ;display:inline-block; text-indent: 20px;"
-                    textwidth="20rem"
-                    beforetext=""
-                    hint=""
-                    :text="recipient.recipientName"
-                  />
-                  <underline-form
-                    :key="recipient.p1DeliveryDate"
-                    style="margin:0.5rem 0 ;display:inline-block; text-indent: 5px;"
-                    textwidth="15rem"
-                    beforetext="on"
-                    :text="recipient.p1DeliveryDate"
-                  />
-                </div>
-              </template>
-            </div>
-
-            <div v-if="electronicRecipients(applicant).length > 0">
-              <div>
-                by sending it/them to the following persons by e-mail, fax or other electronic means
-                to that person:
-              </div>
-              <template v-for="(recipient, i) of electronicRecipients(applicant)">
-                <div :key="i" style="display:inline-block; margin:0.5rem 0;">
-                  <underline-form
-                    :key="recipient.recipientName"
-                    style="margin:0.5rem 0 ;display:inline-block; text-indent: 20px;"
-                    textwidth="20rem"
-                    beforetext=""
-                    hint=""
-                    :text="recipient.recipientName"
-                  />
-                  <underline-form
-                    :key="recipient.p1DeliveryDate"
-                    style="margin:0.5rem 0 ;display:inline-block; text-indent: 5px;"
-                    textwidth="15rem"
-                    beforetext="on"
-                    :text="recipient.p1DeliveryDate"
-                  />
-                </div>
-              </template>
-
-              <div v-if="allP1DeliveryElectronicReceipt(applicant)">
+            </template>
+            <ul style="margin-left:30px"> 
+              <li v-if="allP1DeliveryElectronicReceipt(applicant)">
                 Each of the persons who received delivery by e-mail, fax or other electronic means
                 has, in writing, acknowledged receipt of the document(s) referred to in this
                 section.
-              </div>
-              
-              <div v-if="electronicRecipients(applicant).length <= 1 && allP1DeliveryElectronicReceiptRetain(applicant)">
+              </li>
+                
+              <li v-if="electronicRecipients(applicant).length <= 1 && allP1DeliveryElectronicReceiptRetain(applicant)">
                 I will retain a copy of those acknowledgements until the personal representative of
                 the deceased is discharged and will produce those acknowledgements promptly after 
                 being requested to do so by the registrar.
                 after being requested to do so by the registrar.
-              </div>
+              </li>
 
-              <div v-if="electronicRecipients(applicant).length > 1 && allP1DeliveryElectronicReceiptRetain(applicant)">
+              <li v-if="electronicRecipients(applicant).length > 1 && allP1DeliveryElectronicReceiptRetain(applicant)">
                 We will retain a copy of those acknowledgements until the personal representative of
                 the deceased is discharged and will produce those acknowledgements promptly after
                 being requested to do so by the registrar.
-              </div>
-            </div>
-          </li>
-        </ol>
+              </li>
+            </ul>
+          </div>
+        </div>
 
         <table class="mt-3">
           <tr>
             <td>
-              <div style="margin:0.5rem 0 1rem 0rem;font-weight: 300;font-size:18px;">
+              <div style="margin:0.5rem 0 1rem 0rem;">
                 SWORN (OR AFFIRMED) BEFORE ME
               </div>
-              <underline-form textwidth="10rem" beforetext="at" text="" hint="Commissioner City" />
+              <underline-form textwidth="10rem" beforetext="at" text=""/>
               <div style="margin:0.5rem 0 ; display:inline; text-indent: 5px;">
                 , British Columbia
               </div>
@@ -217,21 +136,13 @@
                 textwidth="18.5rem"
                 beforetext="on"
                 text=""
-                hint="Swear/Affirm Date"
               />
-              <underline-form
-                style="margin:0.5rem 0 ;"
-                textwidth="20rem"
-                beforetext=""
-                text=""
-                hint="A commissioner for taking affidavits for British Columbia"
-              />
-              <div style="margin:0.5rem 0 ;font-size:14px; "></div>
+              <div style="margin:0.5rem 0;"></div>
               <underline-form
                 style="margin:.5rem 0 ;"
                 textwidth="20rem"
                 beforetext=""
-                hint="[print name or affix stamp of commissioner]"
+                hint="A commissioner for taking affidavits for British Columbia"
                 text=""
               />
             </td>
