b0f02d7591ea80a78fc0e80f75e2b64058206018
nikola-rados <nrados@uvic.ca>
2022-02-17 11:16:32 -0800

    Add special check to hide incomplete name answer

--- web/src/survey/survey-templating.ts
+++ web/src/survey/survey-templating.ts
@@ -15,11 +15,13 @@ export function addCustomTemplating(surveyRuntime: any) {
       const data = `${options.name.replace("bullets(", "").replace(")", "")}`;
       const targetName = data.split(".")[0];
       const key = data.split(".")[1];
-      const question = sender.getQuestionByName(targetName)?.value;
-      if (question) {
+      const question = sender.getQuestionByName(targetName);
+      const questionValue = question?.value; 
+      if (questionValue) {
         const bullets = [];
-        question.forEach(function(element) {
-          const value = `${key?.length > 0 ? element[key] : element}`;
+        questionValue.forEach(function(element) {
+          let value = `${key?.length > 0 ? element[key] : element}`;
+          if (["spouseName", "childName"].includes(key) && value.includes("(") && value.includes(")")) value = "";
           if (value && value !== "undefined") bullets.push(`<li>${value}</li>`);
         });
         options.value = bullets.join("\r\n");
