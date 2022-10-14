dc5ef44787a98876b18eab2cc97d5a62f60fe25f
nikola-rados <nrados@uvic.ca>
2022-10-14 11:58:23 -0700

    Update landing page colours & background

--- web/src/components/home/LandingPage.vue
+++ web/src/components/home/LandingPage.vue
@@ -1,84 +1,95 @@
 <template>
-  <b-card v-if="pageReady" id="landing-page" class="container" border-variant="white">
-    <b-row class="mt-3">
-      <b-col>
-        <div 
-        class="text-primary h1"
-        style="text-align:center"
-          >Represent Someone Who Died (also known as Probate)*</div
+  <div class="wrap">
+    <div class="content">
+      <b-card 
+        v-if="pageReady"
+        id="landing-page"
+        class="container"
+        style="background: content-box; border: none; color: black"
         >
-        <div 
-        class="h3"
-        style="text-align:center">
-          <b>This service will allow you to apply for a <tooltip :index="0" title="Representation Grant" /> from the Supreme Court of British Columbia</b>
-        </div>
-        <div style="text-align:center">
-          <b>*At present this application is limited to complete the forms required when the deceased did not leave a will</b>
-        </div>
-        <br/>
-        <br/>
-        <br/>
-        <div
-        class="h3"
-        style="text-align:center">
-          How it Works?
-        </div>
-        <b-list-group-item>
-          <div class="row">
-            <div class="col-md-6">
-              <ol type="1">
-                <li>
-                  Register of login
-                  <br/>
-                  To use this application, you will need to setup a Basic BCeID account. If you do not currently have a Basic BCeID account you can register for one by clicking the Registrar button.
-                </li>
-                <br/>
-                <li>
-                  Answer Questions
-                  <br/>
-                  Our secure online questionnaire will ensure that this service is appropriate for you to use and will save your work so, you can proceed at your own pace.
-                </li>
-              </ol>
+        <b-row class="mt-3">
+          <b-col>
+            <div 
+            class="text-primary h1"
+            style="text-align:center"
+              >Represent Someone Who Died
+              <br>
+              (also known as Probate)*</div
+            >
+            <div
+            class="h3"
+            style="text-align:center">
+              <b>This service will allow you to apply for a <tooltip :index="0" title="Representation Grant" /> from the Supreme Court of British Columbia</b>
             </div>
-            <div class="col-md-6">
-              <ol type="1" start="3">
-                <li>
-                  Print Your Forms
-                  <br/>
-                  Once you're done with the question, you will need to print your documents and file at your local court registry.
-                </li>
-              </ol>
+            <div style="text-align:center">
+              <b>*At present this application is limited to complete the forms required when the deceased did not leave a will</b>
             </div>
-          </div>
-        </b-list-group-item>
-        <b-card border-variant="white">
-          <b-card border-variant="white" v-if="!isLoggedIn">
-            <b-row class="justify-content-center">
-              <b-button
-                variant="primary"
-                size="lg"
-                class="survey-button text-white"
-                @click="navigate('new')"
-              >
-                <strong>New User</strong> - Let’s get started
-              </b-button>
-            </b-row>
+            <br/>
+            <br/>
+            <br/>
+            <div
+            class="h3"
+            style="text-align:center">
+              How it Works?
+            </div>
+            <b-list-group-item>
+              <div class="row" style="font-weight: bold; color: black">
+                <div class="col-md-6">
+                  <ol type="1">
+                    <li>
+                      Register of login
+                      <br/>
+                      To use this application, you will need to setup a Basic BCeID account. If you do not currently have a Basic BCeID account you can register for one by clicking the Registrar button.
+                    </li>
+                    <br/>
+                    <li>
+                      Answer Questions
+                      <br/>
+                      Our secure online questionnaire will ensure that this service is appropriate for you to use and will save your work so, you can proceed at your own pace.
+                    </li>
+                  </ol>
+                </div>
+                <div class="col-md-6">
+                  <ol type="1" start="3">
+                    <li>
+                      Print Your Forms
+                      <br/>
+                      Once you're done with the question, you will need to print your documents and file at your local court registry.
+                    </li>
+                  </ol>
+                </div>
+              </div>
+            </b-list-group-item>
+            <b-card border-variant="white" style="background: content-box; border: none">
+              <b-card border-variant="white" v-if="!isLoggedIn" style="background: content-box; border: none">
+                <b-row class="justify-content-center">
+                  <b-button
+                    variant="primary"
+                    size="lg"
+                    class="survey-button text-white"
+                    @click="navigate('new')"
+                  >
+                    <strong>New User</strong> - Let’s get started
+                  </b-button>
+                </b-row>
 
-            <b-row class="justify-content-center">
-              <b-button
-                variant="warning"
-                class="login-button text-primary"
-                @click="navigate('returning')"
-              >
-                <strong>Returning User?</strong>
-                <br />Log in with BCeID
-              </b-button>
-            </b-row>
-          </b-card>
-        </b-card>
-      </b-col>
-    </b-row>
-  </b-card>
+                <b-row class="justify-content-center">
+                  <b-button
+                    variant="warning"
+                    class="login-button text-primary"
+                    @click="navigate('returning')"
+                  >
+                    <strong>Returning User?</strong>
+                    <br />Log in with BCeID
+                  </b-button>
+                </b-row>
+              </b-card>
+            </b-card>
+          </b-col>
+        </b-row>
+      </b-card>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -112,11 +123,11 @@ export default class LandingPage extends Vue {
 @import "@/styles/_common";
 .list-group-item {
   border-color: white;
-  border: 2px solid rgba($gov-pale-grey, 0.3);
+  border: 2px solid rgba($gov-navy-blue, 0.5);
   border-radius: 18px;
-  background-color: rgba($gov-mid-blue, 0.6);
+  background-color: rgba($gov-mid-blue, 0);
   margin-top: 0.5rem;
-  color: $gov-white;
+  color: $text-color;
 }
 .survey-button {
   color: black;
@@ -141,5 +152,25 @@ export default class LandingPage extends Vue {
   &:active {
     border: 2px solid rgba($gov-white, 0.8);
   }
+}
+.wrap {
+  position: relative;
+}
+
+.wrap:before {
+  content: ' ';
+  display: block;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0.5;
+  background-image: url('/images/legislature-dome.jpg');
+  background-size: cover;
+}
+
+.content  {
+  position: relative;
 }
 </style>
