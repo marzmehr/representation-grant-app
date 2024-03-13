500e14b326925ff9ec6332275bbc038286b35377
marzmehr <mars.mehr@gmail.com>
2024-03-13 12:31:04 -0700

    REPGRANT-419: Add functionality to handle relatedPeople data extraction

--- web/src/filters/relatedPeople.ts
+++ web/src/filters/relatedPeople.ts
@@ -3,7 +3,7 @@ import { creditorOrgInfoType, creditorPersonInfoType } from "@/types/Application
 import { spouseInfoType } from "@/types/Application/Spouse";
 
 
-export function getRelatedSpouses(step){
+export function getRelatedSpouses(step, includePrinciple, includeDescription){
     const related = [];
     let spouseList: spouseInfoType[] = [];
     
@@ -12,29 +12,74 @@ export function getRelatedSpouses(step){
     } 
 
     for (const spouse of spouseList){
-        related.push(spouse.spouseName);
 
-        if(spouse.spouseIsAlive == 'y' && spouse.spouseIsAdult == 'n' &&
-        spouse.spouseHasGuardian == 'y' && spouse.spouseGuardianName?.length>0){
+        // Alive and adult and competent
 
-            related.push(spouse.spouseGuardianName);
+        if(spouse.spouseIsAlive == 'y' && spouse.spouseIsAdult == 'y' && spouse.spouseIsCompetent == 'y'){
+            related.push(spouse.spouseName);
+        }
+        
+        // Alive and minor
+
+        if(spouse.spouseIsAlive == 'y' && spouse.spouseIsAdult == 'n'){
+
+            if(spouse.spouseHasGuardian == 'y' && spouse.spouseGuardianName?.length>0){
+
+                if(includeDescription)
+                    related.push(spouse.spouseGuardianName + ' ('+ spouse.spouseName + "'s Guardian)");
+                else
+                    related.push(spouse.spouseGuardianName);
+                if(includePrinciple){
+                    related.push(spouse.spouseName);
+                }
+            } else {
+                related.push(spouse.spouseName);
+            }
+        }        
+
+        //Alive and adult and incompetent
+
+        if(spouse.spouseIsAlive == 'y' && spouse.spouseIsAdult == 'y' && spouse.spouseIsCompetent == 'n'){
+
+            if(spouse.spouseHasNominee == 'y' && spouse.spouseNomineeName?.length>0){
+
+                if(includeDescription)
+                    related.push(spouse.spouseNomineeName + ' ('+ spouse.spouseName + "'s Nominee)");
+                else
+                    related.push(spouse.spouseNomineeName);
+
+                if(spouse.spouseNomineeFormal == 'n' || (spouse.spouseNomineeFormal == 'y' && includePrinciple)){
+                    related.push(spouse.spouseName);
+                }
+            } else if(spouse.spouseHasNominee == 'n'){
+                related.push(spouse.spouseName);
+            }
+        } 
+        //Deceased after
+
+        if(spouse.spouseIsAlive == 'n'){
+
+            if(spouse.spouseDied5DaysAfter == 'y'){
+
+                if(spouse.spouseHasPersonalRep == 'y' && spouse.spousePersonalRepName?.length>0){
+
+                    if(includeDescription)
+                        related.push(spouse.spousePersonalRepName + ' ('+ spouse.spouseName + "'s Personal Representative)");
+                    else
+                        related.push(spouse.spousePersonalRepName);
+                }
+
+            } else if(spouse.spouseDied5DaysAfter == 'n' && includePrinciple){
+                related.push(spouse.spouseName);
+            }           
         }
 
-        if(spouse.spouseIsAlive == 'y' && spouse.spouseIsAdult == 'y' &&
-            spouse.spouseIsCompetent == 'n' && spouse.spouseHasNominee == 'y' && spouse.spouseNomineeName?.length>0){
-            related.push(spouse.spouseNomineeName);
-        }
-
-        if(spouse.spouseIsAlive == 'n' && spouse.spouseDied5DaysAfter == 'y' &&
-        spouse.spouseHasPersonalRep == 'y' && spouse.spousePersonalRepName?.length>0){
-            related.push(spouse.spousePersonalRepName);
-        }
     }
     return related;
 }
 
 
-export function getRelatedChildren(step){
+export function getRelatedChildren(step, includePrinciple, includeDescription){
     const related = [];
     let childList: childDetailsDataInfoType[] = [];
     if (step.result?.childrenSurvey?.data) {
@@ -42,29 +87,74 @@ export function getRelatedChildren(step){
     } 
 
     for (const child of childList){
-        related.push(child.childName);
 
-        if(child.childIsAlive == 'y' && child.childIsAdult == 'n' &&
-            child.childHasGuardian == 'y' && child.childGuardianName?.length>0){
+        // Alive and adult and competent
 
-            related.push(child.childGuardianName);
+        if(child.childIsAlive == 'y' && child.childIsAdult == 'y' && child.childIsCompetent == 'y'){
+            related.push(child.childName);
+        }        
+
+        // Alive and minor
+
+        if(child.childIsAlive == 'y' && child.childIsAdult == 'n'){
+
+            if(child.childHasGuardian == 'y' && child.childGuardianName?.length>0){
+
+                if(includeDescription)
+                    related.push(child.childGuardianName + ' ('+ child.childName + "'s Guardian)");
+                else
+                    related.push(child.childGuardianName);
+                if(includePrinciple){
+                    related.push(child.childName);
+                }
+            } else {
+                related.push(child.childName);
+            }
+        } 
+
+        //Alive and adult and incompetent
+
+        if(child.childIsAlive == 'y' && child.childIsAdult == 'y' && child.childIsCompetent == 'n'){
+
+            if(child.childHasNominee == 'y' && child.childNomineeName?.length>0){
+
+                if(includeDescription)
+                    related.push(child.childNomineeName + ' ('+ child.childName + "'s Nominee)");
+                else
+                    related.push(child.childNomineeName);
+
+                if(child.childNomineeFormal == 'n' || (child.childNomineeFormal == 'y' && includePrinciple)){
+                    related.push(child.childName);
+                }
+            } else if(child.childHasNominee == 'n'){
+                related.push(child.childName);
+            }
+        } 
+        //Deceased after
+
+        if(child.childIsAlive == 'n'){
+
+            if(child.childDiedAfter == 'y'){
+
+                if(child.childHasPersonalRep == 'y' && child.childPersonalRepName?.length>0){
+
+                    if(includeDescription)
+                        related.push(child.childPersonalRepName + ' ('+ child.childName + "'s Personal Representative)");
+                    else
+                        related.push(child.childPersonalRepName);
+                }
+
+            } else if(child.childDiedAfter == 'n' && includePrinciple){
+                related.push(child.childName);
+            }           
         }
-
-        if(child.childIsAlive == 'y' && child.childIsAdult == 'y' &&
-            child.childIsCompetent == 'n' && child.childHasNominee == 'y' && child.childNomineeName?.length>0){
-            related.push(child.childNomineeName);
-        }
-
-        if(child.childIsAlive == 'n' && child.childDiedAfter == 'y' &&
-            child.childHasPersonalRep == 'y' && child.childPersonalRepName?.length>0){
-            related.push(child.childPersonalRepName);
-        }
+       
     }
     return related;
 }
 
 
-export function getRelatedCreditor(step){
+export function getRelatedCreditor(step, includePrinciple, includeDescription){
     const related = [];
     let creditorPersonList: creditorPersonInfoType[] = [];
     if (step.result?.creditorSurvey?.data) {
@@ -72,26 +162,62 @@ export function getRelatedCreditor(step){
     } 
 
     for (const creditorPerson of creditorPersonList){
-        related.push(creditorPerson.creditorPersonName);
 
-        if(creditorPerson.creditorPersonIsAlive == 'y' && creditorPerson.creditorPersonIsAdult == 'n' &&
-            creditorPerson.creditorPersonHasGuardian == 'y' && creditorPerson.creditorPersonGuardianName?.length>0){
+        // Alive and adult and competent
 
-                related.push(creditorPerson.creditorPersonGuardianName);
+        if(creditorPerson.creditorPersonIsAlive == 'y' && creditorPerson.creditorPersonIsAdult == 'y' && creditorPerson.creditorPersonIsCompetent == 'y'){
+            related.push(creditorPerson.creditorPersonName);
+        }
+        
+        // Alive and minor
+
+        if(creditorPerson.creditorPersonIsAlive == 'y' && creditorPerson.creditorPersonIsAdult == 'n'){
+
+            if(creditorPerson.creditorPersonHasGuardian == 'y' && creditorPerson.creditorPersonGuardianName?.length>0){
+
+                if(includeDescription)
+                    related.push(creditorPerson.creditorPersonGuardianName + ' ('+ creditorPerson.creditorPersonName + "'s Guardian)");
+                else
+                    related.push(creditorPerson.creditorPersonGuardianName);
+                if(includePrinciple){
+                    related.push(creditorPerson.creditorPersonName);
+                }
+            } else {
+                related.push(creditorPerson.creditorPersonName);
+            }
+        }        
+
+        //Alive and adult and incompetent
+
+        if(creditorPerson.creditorPersonIsAlive == 'y' && creditorPerson.creditorPersonIsAdult == 'y' && creditorPerson.creditorPersonIsCompetent == 'n'){
+
+            if(creditorPerson.creditorPersonHasNominee == 'y' && creditorPerson.creditorPersonNomineeName?.length>0){
+
+                if(includeDescription)
+                    related.push(creditorPerson.creditorPersonNomineeName + ' ('+ creditorPerson.creditorPersonName + "'s Nominee)");
+                else
+                    related.push(creditorPerson.creditorPersonNomineeName);
+
+                if(creditorPerson.creditorPersonNomineeFormal == 'n' || (creditorPerson.creditorPersonNomineeFormal == 'y' && includePrinciple)){
+                    related.push(creditorPerson.creditorPersonName);
+                }
+            } else if(creditorPerson.creditorPersonHasNominee == 'n'){
+                related.push(creditorPerson.creditorPersonName);
+            }
+        } 
+        //Deceased after
+
+        if(creditorPerson.creditorPersonIsAlive == 'n'){
+           
+            if(creditorPerson.creditorPersonHasPersonalRep == 'y' && creditorPerson.creditorPersonPersonalRepName?.length>0){
+
+                if(includeDescription)
+                    related.push(creditorPerson.creditorPersonPersonalRepName + ' ('+ creditorPerson.creditorPersonName + "'s Personal Representative)");
+                else
+                    related.push(creditorPerson.creditorPersonPersonalRepName);
+            }                 
         }
 
-        if(creditorPerson.creditorPersonIsAlive == 'y' && creditorPerson.creditorPersonIsAdult == 'y' &&
-            creditorPerson.creditorPersonIsCompetent == 'n' && creditorPerson.creditorPersonHasNominee == 'y' && 
-            creditorPerson.creditorPersonNomineeName?.length>0){
-            
-                related.push(creditorPerson.creditorPersonNomineeName);
-        }
-
-        if(creditorPerson.creditorPersonIsAlive == 'n' && creditorPerson.creditorPersonHasPersonalRep == 'y' && 
-            creditorPerson.creditorPersonPersonalRepName?.length>0){
-
-                related.push(creditorPerson.creditorPersonPersonalRepName);
-        }
     }
     return related;
 }
